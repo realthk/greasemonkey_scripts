0e56b054e327c14702a1f62ee0e595668b456cee
Henrik Tóth <realthk@gmail.com>
2021-09-23 21:15:56 +0200

    #akcio

--- prohardver.js
+++ prohardver.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Prohardver fórum szinezgetés
-// @namespace   https://github.com/realthk/greasemonkey_scripts
+// @namespace   My
 // @description Prohardver fórum szinezgetés
 // @include     https://prohardver.hu/tema/*
 // @include     https://logout.hu/tema/*
@@ -11,20 +11,25 @@
 // @grant       none
 // ==/UserScript==
 var ezvagyoken = "Degeczi";
+var akcio = "#AKCI";
 var ezazenszinem = "#CCFFFF";
 var ezavalaszolokszine = "#CCFF99";
+var akcioszin = "#FFFFFF";
 var main = document.getElementsByClassName("msg-list msg-list-desc");
 var messages = main[0].getElementsByClassName("list-unstyled")[0].getElementsByClassName("media");
 for (var i = 0 ; i < messages.length; i++)
 {
   var message = messages[i].getElementsByClassName("card")[0];
   var haveHit = false;
+  var haveAkcio = false;
 
   if (message !== undefined) {
     var face = message.getElementsByClassName("msg-head-author")[0];
     if (face.innerHTML.indexOf(ezvagyoken)>-1) {
       message.getElementsByClassName("card-body")[0].style.backgroundColor = ezazenszinem;
       haveHit = true;
+    } else if (face.innerHTML.indexOf(akcio)>-1) {
+      message.getElementsByClassName("card-body")[0].style.backgroundColor = ezazenszinem;
     }
     if (!haveHit) {
       var head = message.getElementsByClassName("msg-head-replied");
@@ -41,6 +46,12 @@ for (var i = 0 ; i < messages.length; i++)
           if (row.innerHTML.indexOf(ezvagyoken)>-1) {
             row.style.backgroundColor = ezavalaszolokszine;
           }
+          if (row.innerHTML.toUpperCase().indexOf(akcio)>-1) {
+            haveAkcio = true;
+          }
+        }
+        if (haveAkcio) {
+            message.getElementsByClassName("card-body")[0].style.backgroundColor = akcioszin;
         }
       }
     }
